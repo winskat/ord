c094c5fda8392ad47b664fa2f4fcb10d6b4f28f5
Greg Martin <gm7t2@gmail.com>
2023-04-12 14:33:59 +0000

    Show the sat number and inscription number in `wallet inscriptions` output.

--- src/subcommand/wallet/inscriptions.rs
+++ src/subcommand/wallet/inscriptions.rs
@@ -2,6 +2,8 @@ use {super::*, crate::wallet::Wallet};
 
 #[derive(Serialize, Deserialize)]
 pub struct Output {
+  pub sat: Sat,
+  pub number: u64,
   pub inscription: InscriptionId,
   pub location: SatPoint,
   pub explorer: String,
@@ -25,7 +27,12 @@ pub(crate) fn run(options: Options) -> Result {
 
   for (location, inscription) in inscriptions {
     if unspent_outputs.contains_key(&location.outpoint) {
+      let entry = index
+        .get_inscription_entry(inscription)?
+        .ok_or_else(|| anyhow!("Inscription {inscription} not found"))?;
       output.push(Output {
+        sat: entry.sat.unwrap(),
+        number: entry.number,
         location,
         inscription,
         explorer: format!("{explorer}{inscription}"),
